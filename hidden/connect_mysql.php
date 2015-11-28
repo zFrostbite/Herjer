2daee68514277b60000ac53717301d86fc6baba9
zFrostbite <frostbite@1337.no>
2015-11-28 20:10:21 +0100

    asfejhkl
    f<asdfgs<

---
+++ hidden/connect_mysql.php
@@ -0,0 +1,17 @@
+<?php
+$servername 	= "127.0.0.1";
+$password 		= "";
+$user			= "root";
+$db				= "HerjerMMA";
+
+// Kobla te MySQL DB
+$conn = new mysqli($servername, $user, $password, $db);
+
+// Sjekk_
+if($conn->connect_error) {
+	die("Tilkobling til MySQL database ikke vellykket <br/>" . "ERRNO: " . $conn->connect_errno ."<br/> " . $conn->connect_error);
+}
+
+
+echo("Tilkobling vellykket. MySQL AKTIV! Tilkoblet: " .$db);
+?>
